6cddd08ea9b56c3d23d803647c70bb3c637d1363
Hemant Kumar Singh <hemantkumarsingh@administrators-MacBook-Pro-4.local>
2020-04-27 16:18:21 +0530

    cleaned the repo

--- src/com/aruba/arch/CleaningTraits.scala
+++
@@ -1,9 +0,0 @@
-package com.aruba.arch
-
-import org.apache.spark.sql.DataFrame
-import org.apache.spark.sql.SparkSession
-trait CleaningTraits {
-  def invalidLocationFilter(inputDf : DataFrame , spark :SparkSession) : DataFrame
-  def detectDataLossFilter(inputDf : DataFrame, spark :SparkSession,yearOfProcessing:Int) : DataFrame
-  def dropDataDuplicate(inputDf : DataFrame, spark :SparkSession) : DataFrame
-}
